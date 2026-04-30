cb7e75591cd606cb24dbf32ba40525c13bfad2c7
SOHAM GHOSH <sohamghosh1762@gmail.com>
2026-04-30 13:48:07 +0530

    Change app.run to use environment variables for host and port

--- app.py
+++ app.py
@@ -217,4 +217,5 @@ def api_ping():
 # RUN APP
 # ---------------------------------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
